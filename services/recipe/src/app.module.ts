fcdcd0acaeefbdf68063b30750b1555c7b6ba273
Robert Dey <dey@kowalzik.biz>
2020-05-25 23:10:48 +0200

    make db connection configurable

--- services/recipe/src/app.module.ts
+++ services/recipe/src/app.module.ts
@@ -1,15 +1,19 @@
-import { RecipeModule } from './recipe/recipe.module';
-import { DatabaseConfigService } from './database-config.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { RecipeModule } from './recipe/recipe.module';
+import { DatabaseConfigService } from './database-config.service';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { RecipeService } from './recipe/recipe.service';
-import { RecipeController } from './recipe/recipe.controller';
 
 @Module({
   imports: [
-    TypeOrmModule.forRootAsync({ useClass: DatabaseConfigService }),
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useClass: DatabaseConfigService,
+      inject: [ConfigService],
+    }),
     RecipeModule
   ],
   controllers: [AppController],
--- services/recipe/src/database-config.service.ts
+++ services/recipe/src/database-config.service.ts
@@ -1,16 +1,20 @@
 import { Injectable } from '@nestjs/common';
 import { TypeOrmOptionsFactory, TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class DatabaseConfigService implements TypeOrmOptionsFactory {
+
+  constructor(private readonly config: ConfigService) {}
+
   createTypeOrmOptions(): TypeOrmModuleOptions {
     return {
       type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'localdev',
-      database: 'recipe',
+      host: this.config.get('DATABASE_HOST'),
+      port: this.config.get('DATABASE_PORT'),
+      username: this.config.get('DATABASE_USER'),
+      password: this.config.get('DATABASE_PASSWORD'),
+      database: this.config.get('DATABASE_NAME'),
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
       synchronize: true,
     };
